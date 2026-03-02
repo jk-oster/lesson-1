af2701664422fd46e519c7a20f1e6e4ccd68a330
Jakob Osterberger <jo@studiomitte.com>
2026-03-02 12:47:03 +0100

    [FEATURE] Implement Observable class and update observable function

--- kwm-js/observable.js
+++ kwm-js/observable.js
@@ -1,5 +1,9 @@
 'use strict';
 
+class Observable {
+
+}
+
 /**
  * Creates a new observable value.
  * @template T
@@ -7,9 +11,7 @@
  * @returns {{ value: T, subscribe: (fn: () => void) => () => boolean }}
  * @example
  * const count = observable(0);
- * count.value; // 0
- * count.value = 1; // triggers subscribers
+ * count.get(); // 0
+ * count.set(1); // triggers subscribers
  */
-export function observable(initialValue) {
-
-}
+export const observable = (initialValue) => new Observable(initialValue);
